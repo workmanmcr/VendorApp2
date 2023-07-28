2ec159523a582b680bc3e049cb9d77bb8bd4aa30
workmanmcr <workmanmcr@gmail.com>
2023-07-27 20:13:16 -0700

    fix pathing issues

--- VendorApp/Controllers/VendorController.cs
+++ VendorApp/Controllers/VendorController.cs
@@ -8,63 +8,63 @@ namespace VendorApp.Controllers
 
   public class VendorController : Controller
   {
-    [HttpGet("/Vendor")]
+    [HttpGet("/vendor")]
     public ActionResult Index()
     {
       List<Vendor> vendors = Vendor.GetAll();
       return View(vendors);
     }
-    [HttpPost("/Vendor")]
+    [HttpPost("/vendor")]
     public ActionResult Create(string name, string description)
     {
       new Vendor(name, description);
       List<Vendor> vendors = Vendor.GetAll();
       return RedirectToAction("Index", vendors);
     }
-    [HttpGet("/Vendor/New")]
+    [HttpGet("/vendor/new")]
     public ActionResult New()
     {
       return View();
     }
-    [HttpGet("/Vendor/{id}")]
+    [HttpGet("/vendor/{id}")]
     public ActionResult Details(int id)
     {
       Vendor vendor = Vendor.GetVendorWithId(id);
       return View(vendor);
     }
-    [HttpGet("/Vendor/{id}/Order/New")]
+    [HttpGet("/vendor/{id}/order/new")]
     public ActionResult newOrder(int id)
     {
       Vendor vendor = Vendor.GetVendorWithId(id);
       return View(vendor);
     }
-    [HttpPost("/Vendor/{id}/Order/New")]
+    [HttpPost("/vendor/{id}/order/new")]
     public ActionResult CreateOrder(string title, string description, int price, DateTime orderDate, int vendorId)
     {
       Vendor vendor = Vendor.GetVendorWithId(vendorId);
       vendor.AddOrder(new Order(title, description, price, vendor.GetOrderCount(), vendor.Name, orderDate, vendor.Id));
-      return RedirectToAction("Index");
+      return RedirectToAction("Details");
     }
-    [HttpGet("/Vendor/{id}/Order/{oid}")]
+    [HttpGet("/vendor/{id}/order/{oid}")]
     public ActionResult ShowOrder(int id, int oid)
     {
       Vendor vendor = Vendor.GetVendorWithId(id);
       Order order = vendor.GetOrderWithId(oid);
       return View(order);
     }
+   
 
-
-    [HttpGet("/Vendor/Search")]
+    [HttpGet("/vendor/search")]
     public ActionResult Search()
     {
 
       return View();
     }
-    [HttpPost("/Vendor/Search")]
+    [HttpPost("/vendor/search")]
     public ActionResult SearchVendor(string vendor)
     {
       Vendor vendor1 = Vendor.SearchVendor(vendor);
-
+      
       return View("Details", vendor1);
     }
 
